94f4e86b6bfc0e872b57104a7cc5892784e7caa2
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-10-06 15:10:47 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState,useEffect } from 'react'
 import './App.css'
-
+import sunny from './asset/sunBGRemove.png'
+import rain from './asset/rain.png'
+import could from './asset/couldyBG.png'
+import snow from './asset/snowBG.png'
 const App = () => {
   const [data,setData] = useState()
   const [searchData, setSearchData] = useState()
@@ -10,11 +13,12 @@ const App = () => {
 //step 3 search box to show data in console -mainBox
 
 const searchCityFunc =()=>{
-  // useEffect(()=>{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchData}&appid=82c0389cbf40503da8bbad129031d570`)
     .then(response => response.json())
-    .then(data => setData(data))
-  // },[])
+    .then(data => {
+      setData(data);
+      console.log(data)
+    })
 }
   return (
     <div className='mainBox'>
@@ -33,16 +37,22 @@ const searchCityFunc =()=>{
 
 </div>
 
-<div className='weatherBox'>
-  <h2>Today</h2>
-  <p>Temperature:{data.main.temp}</p>
-  <p>Maximum:{data.main.temp_max}</p>
-  <p>Minumum:{data.main.temp_min}</p>
-  <p>Weather:{data.weather[0].main}</p>
-</div>
-
+{data && 
+  <div className='weatherBox'>
+    <h2>Today</h2>
+    <p>Temperature:{Math.trunc(data.main.temp)}</p>
+    <p>Maximum:{parseInt(data.main.temp_max)}</p>
+    <p>Minumum:{data.main.temp_min}</p>
+    <p>Weather:{data.weather[0].main}</p>
+    <img src={sunny} alt="Image not working"/>
+  </div>
+}
     </div>
   )
 }
 
 export default App
+
+
+
+
